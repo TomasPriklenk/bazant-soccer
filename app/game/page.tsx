0f909af9f0c06a91b0cf035b5db2e0292eacb708
TomasPriklenk <fcbazantnice@gmail.com>
2026-01-13 15:22:35 +0100

    Create page.tsx

---
+++ app/game/page.tsx
@@ -0,0 +1,8 @@
+export default function Game() {
+  return (
+    <div style={{ color: "white", padding: 40 }}>
+      <h1>⚽ Bažant</h1>
+      <p>Jsi přihlášen. Tady bude hra.</p>
+    </div>
+  );
+}
